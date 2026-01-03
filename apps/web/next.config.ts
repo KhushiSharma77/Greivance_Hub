8d79678f51b48497a0959d8f4278d60064c0fe68
Harish-Naruto <harishnarote7@gmail.com>
2026-01-03 11:22:38 +0530

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@team-call-of-code/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
