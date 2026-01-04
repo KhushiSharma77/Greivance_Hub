10fd8b0bc1c77bb6e3b7179c6b41fdea4be42ac5
Harish-Naruto <harishnarote7@gmail.com>
2026-01-04 11:20:48 +0530

    fixed unresolved conflict in worker

--- apps/server/src/worker/src/queues/grievance.queue.ts
+++ apps/server/src/worker/src/queues/grievance.queue.ts
@@ -1,16 +1,9 @@
-/**
- * Grievance Queue
- * Handles grievance processing jobs
- */
-
-// TODO: Implement BullMQ queue for grievance processing
 import { Queue,Worker } from "bullmq";
 import { redisConnection } from "../utils/redis";
 import { grievanceProcessor } from "../processors/grievance.processor";
 
 export const GRIEVANCE_QUEUE_NAME = "grievance-processing";
 
-// TODO: Create and export queue instance
 export const grievanceQueue = new Queue(GRIEVANCE_QUEUE_NAME, {
   connection: redisConnection,
 });
