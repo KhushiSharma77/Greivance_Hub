7a97ce7e0348574a21202c61782ee35556111b4e
Khushi Sharma <khushisharma4628@gmail.com>
2026-05-14 01:06:45 +0530

    redis connection test

--- apps/server/src/lib/redisconnection.ts
+++ apps/server/src/lib/redisconnection.ts
@@ -1,9 +1,16 @@
 import { Redis } from 'ioredis';
 
-export const redis = process.env.REDIS_URL 
+export const redis = process.env.REDIS_URL
   ? new Redis(process.env.REDIS_URL, { maxRetriesPerRequest: null })
   : new Redis({
-      host: '127.0.0.1',
-      port: 6379,
-      maxRetriesPerRequest: null,
-    });
+    host: '127.0.0.1',
+    port: 6379,
+    maxRetriesPerRequest: null,
+  });
+redis.on('connect', () => {
+  console.log('✅ Redis connected');
+});
+
+redis.on('error', (err) => {
+  console.error('❌ Redis connection error:', err);
+});
