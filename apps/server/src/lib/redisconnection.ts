012d5b6800c8ca9e7f3ff9a1ef9abddc5b285c38
Khushi Sharma <khushisharma4628@gmail.com>
2026-05-14 00:55:05 +0530

    redis database

--- apps/server/src/lib/redisconnection.ts
+++ apps/server/src/lib/redisconnection.ts
@@ -1,8 +1,9 @@
 import { Redis } from 'ioredis';
 
-export const redis = new Redis({
-  host: '127.0.0.1',
-  port:  6379,
-  maxRetriesPerRequest: null,
-
-});
+export const redis = process.env.REDIS_URL 
+  ? new Redis(process.env.REDIS_URL, { maxRetriesPerRequest: null })
+  : new Redis({
+      host: '127.0.0.1',
+      port: 6379,
+      maxRetriesPerRequest: null,
+    });
