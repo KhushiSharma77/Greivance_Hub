33a3c70561363a36e232117b91fae5603245dcb0
Khushi Sharma <khushisharma4628@gmail.com>
2026-05-14 10:04:46 +0530

    redis fiix

--- apps/server/src/worker/src/utils/redis.ts
+++ apps/server/src/worker/src/utils/redis.ts
@@ -1,8 +1,12 @@
 import { Redis } from 'ioredis';
 
-export const redisConnection = new Redis({
-  host: '127.0.0.1',
-  port:  6379,
-  maxRetriesPerRequest: null,
-
-});
+export const redisConnection = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: null,
+      tls: process.env.REDIS_URL.startsWith('rediss://') ? { rejectUnauthorized: false } : undefined,
+    })
+  : new Redis({
+      host: '127.0.0.1',
+      port: 6379,
+      maxRetriesPerRequest: null,
+    });
